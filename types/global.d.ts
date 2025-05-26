8ab6a2f7f654d8151695889841f8b31ef81f834a
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-05-26 05:19:39 -0500

    feat: add global types for JWT session claims and SDK configuration interfaces
    - Introduced `CustomJwtSessionClaims` interface to extend JWT session claims with metadata and user information.
    - Created `ChipiSDKConfig`, `WalletData`, and `TransferParams` interfaces to define the structure for SDK configuration and wallet operations.

---
+++ types/global.d.ts
@@ -0,0 +1,11 @@
+export {};
+declare global {
+  interface CustomJwtSessionClaims {
+    metadata: {
+      walletCreated?: boolean;
+      publicKey?: string;
+      encryptedPrivateKey?: string;
+    };
+    firstName?: string;
+  }
+}
